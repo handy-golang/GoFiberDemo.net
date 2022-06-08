487ff9be51c8386b17c16afe0a06275d4a4e4270
墨七 <meichangliang@mo7.cc>
2022-06-08 19:17:51 +0800

    exit-push

--- server/router/midst/ping.go
+++ server/router/midst/ping.go
@@ -4,6 +4,7 @@ import (
 	"GoFiberDemo.net/server/global/config"
 	"GoFiberDemo.net/server/router/result"
 	"github.com/EasyGolang/goTools/mRes/mFiber"
+	"github.com/EasyGolang/goTools/mStr"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -22,7 +23,11 @@ func Ping(c *fiber.Ctx) error {
 	// 获取 token
 	token := c.Get("Token")
 	if len(token) > 0 {
-		// 在这里解析 Token,解析正确则返回,否则不返回
+		// Token 验证
+		_, err := TokenAuth(c)
+		if err != nil {
+			return c.JSON(result.ErrToken.WithData(mStr.ToStr(err)))
+		}
 		ReturnData["Token"] = token
 		return c.JSON(result.OK.WithData(ReturnData))
 	} else {
