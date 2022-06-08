4accdc159d291659cee6d30dc76f55f439fd3d7b
墨七 <meichangliang@mo7.cc>
2022-06-09 06:17:33 +0800

    exit-push

--- server/global/envInit.go
+++ server/global/envInit.go
@@ -32,13 +32,12 @@ func SysEnvInt() {
 func AppEnvInt() {
 	// 检查配置文件在不在
 	isUserEnvPath := mPath.Exists(config.File.AppEnv)
-	if isUserEnvPath {
-		config.LoadAppEnv()
+	if !isUserEnvPath {
+		errStr := fmt.Errorf("没找到 app_env.yaml 配置文件")
+		panic(errStr)
 	}
 
-	if config.AppEnv.Port < 80 {
-		config.AppEnv.Port = 9876
-	}
+	config.LoadAppEnv()
 
 	Log.Println("加载 AppEnv : ", mJson.JsonFormat(mJson.ToJson(config.AppEnv)))
 }
